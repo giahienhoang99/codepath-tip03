3b412f5bed1df451a91c599ad1c7b2730030adca
hienhg <giahienhoang99@gmail.com>
2024-06-11 10:11:26 +0700

    LinkedListCycle brute force solution added

--- week2-Linkedlist-Strings-Arrays/linked_list_cycle.java
+++ week2-Linkedlist-Strings-Arrays/linked_list_cycle.java
@@ -28,4 +28,31 @@ public class Solution {
 
         return false;
     }
+
+    public boolean hasCycleBruteForce(ListNode head) {
+        // edge cases
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        // create set for containing visited nodes
+        Set<ListNode> visited = new HashSet<>();
+
+        // create a ListNode
+        ListNode current = head;
+
+        // traverse linkedlist
+        while (current != null) {
+            if (!visited.contains(current)) {
+                // track visited nodes by adding to the set
+                visited.add(current);
+                // shift the current node forward
+                current = (current.next != null) ? current.next : null;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
