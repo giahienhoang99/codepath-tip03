a6707a03629ef4332de34005b16743a49195d6a2
hienhg <giahienhoang99@gmail.com>
2024-06-17 10:32:15 +0700

    optimized solution for set matrix zeroes

--- week2-Linkedlist-Strings-Arrays/set_matrix_zeroes.java
+++ week2-Linkedlist-Strings-Arrays/set_matrix_zeroes.java
@@ -9,7 +9,7 @@ class Solution {
             matrix[i][col] = 0;
         }
     }
-    public void setZeroes(int[][] matrix) {
+    public void setZeroesHashSet(int[][] matrix) {
         Set<Integer> cols = new HashSet<>();
         Set<Integer> rows = new HashSet<>();
         for (int i = 0; i < matrix.length; i++) {
@@ -29,6 +29,52 @@ class Solution {
         }
         for (int row : rows) {
             setRowZero(matrix, row);
+        }
+    }
+
+    public void setZeroesMarker(int[][] matrix) {
+        // 1) Is first row/col zero?
+        // 2) Add markers (first row/col) based on cell values
+        // 3) Zero out the cells based on the markers in step2
+        // 4) Handle first row/col
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        // 1) Is first row/col zero?
+        boolean first_row_has_zero = false;
+        boolean first_col_has_zero = false;
+        for (int i = 0; i < cols; i++) {
+            if (matrix[0][i] == 0) first_row_has_zero = true;
+        }
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) first_col_has_zero = true;
+        }
+
+        // 2) Add markers (first row/col) based on cell values
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // 3) Zero out the cells based on the markers in step2
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // 4) Handle first row/col
+        for (int col = 0; col < cols; col++) {
+            if (first_row_has_zero) matrix[0][col] = 0;
+        }
+        for (int row = 0; row < rows; row++) {
+            if (first_col_has_zero) matrix[row][0] = 0;
         }
     }
 }
