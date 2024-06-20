bfd9d73c589b1d31b31e98a561375fc42dda8c8a
hienhg <giahienhoang99@gmail.com>
2024-06-20 11:10:16 +0700

    add solutions

---
+++ my-app/src/week5_stacks_queues_trees/my_problems/LowestCommonAncestorOfBST.java
@@ -0,0 +1,18 @@
+package week5_stacks_queues_trees.my_problems;
+
+public class LowestCommonAncestorOfBST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p , q);
+        TreeNode right = lowestCommonAncestor(root.right, p ,q);
+
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            return root;
+        }
+    }
+}
