7300cb0e2dfa82119840c9297a869493345540d7
hienhg <giahienhoang99@gmail.com>
2024-06-25 11:09:35 +0700

    optimized solution using 2ptr for 3sum

--- my-app/src/week3_heaps_hashtables/ThreeSum.java
+++ my-app/src/week3_heaps_hashtables/ThreeSum.java
@@ -3,6 +3,35 @@ package week3_heaps_hashtables;
 import java.util.*;
 
 public class ThreeSum {
+    // Time: O(N^2)
+    // Space: O(1)
+    public static List<List<Integer>> threeSumOptimized(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            // Skip duplicates for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = nums.length - 1;
+            // Two Pointer
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    result.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[k])));
+                    // skip nums[j] duplicates
+                    j++;
+                    while (j < k && nums[j] == nums[j-1]) j++;
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return result;
+    }
+
     public static List<List<Integer>> threeSumHashMap(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
@@ -39,7 +68,10 @@ public class ThreeSum {
         int[] test2 = {0,1,1};
         int[] test3 = {0,0,0};
         System.out.println(threeSumHashMap(test1)); // [[-1, 0, 1], [-1, -1, 2]]
+        System.out.println(threeSumOptimized(test1)); // [[-1, 0, 1], [-1, -1, 2]]
         System.out.println(threeSumHashMap(test2)); // []
+        System.out.println(threeSumOptimized(test2)); // []
         System.out.println(threeSumHashMap(test3)); // [[0, 0, 0]]
+        System.out.println(threeSumOptimized(test3)); // [[0, 0, 0]]
     }
 }
